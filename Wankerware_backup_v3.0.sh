240d93048d551a73d0b8a9fafb496e92c3cfa1e6
wankercraft.org <wankercraft@users.noreply.github.com>
2014-09-29 23:07:30 -0500

    Updating Git to current build

--- Wankerware_backup_v3.0.sh
+++ Wankerware_backup_v3.0.sh
@@ -11,35 +11,6 @@
 #
 #
 #********************VARIABLES********************
-#
-#********************REQUIRES MINECRAFT_OVERVIEWER******************
-#********************MAP MAKING VARIABLES********************
-#This variable defines the directory for your map making program
-MAPPER="/home/dachroni/minecraft/Minecraft_Overviewer/"
-
-#Set to 1 if you want to run the map making portion of the script
-MAP=0
-
-#Set this variable to the primary folder to output you map to
-MAPDIR="/home/dachroni/public_html/map/"
-
-#Set this variable to the primary folder you want the cache files saved to
-MAPCACHE="/home/dachroni/public_html/map/cache/"
-
-#Set this variable to your primary world directory
-WORLD="/home/dachroni/minecraft/wankercraft/wankercraft/"
-
-#**********THERE VARIABLES ARE FOR MULTIPLE WORLDS NEEDING MAPS ALSO (YOU CAN USE MAKE AS MANY AS YOU LIKE)*********
-
-#Set this variable to the secondary folder to output you second world map to
-MAPDIRALT="/home/dachroni/public_html/map2/"
-
-#Set this variable to the secondary folder you want the second world cache files saved to
-MAPCACHEALT="/home/dachroni/public_html/map2/cache/"
-
-#Set this variable to your secondary world directory
-WORLDALT="/home/dachroni/minecraft/wankercraft/didgeridoo/"
-
 
 #********************SERVER BACKUP VARIABLES********************
 
@@ -64,14 +35,6 @@ SAVE=1
 #Name of the screen session the server is running in
 SCREEN="smp"
 
-#********************CRON SCRIPT********************
-
-
-#Set to 1 of you want to add a new entry to your crontab (if allowed)
-#CRON=1
-
-#Set for how often to backup in minutes ie.. 60, 120, etc... (Recommend every 6 hours at most. The files can get large.)
-#BACKUPTIME=360
 
 #********************SERVER BACKUP SCRIPT********************
 
@@ -127,6 +90,25 @@ if [ $BACKUP -eq 1 ]
 		echo Compressing current Wankercraft directory
 			cd /home/dachroni/minecraft/backup/
 			$TAR
+#			sleep 1
+#			tar czf herpderp.tar.gz /home/dachroni/minecraft/backup/wankercraft_main
+######################## connect via scp ############################################
+#spawn scp -p "wankerpants@wankercraft.tk:/home/wankerpants/" herpderp.tar.gz
+#####################
+#	expect {
+#		-re ".*es.*o.*" {
+#			exp_send "yes\r"
+#			exp_continue
+#		}
+#		-re ".*sword.*" {
+#			exp_send "qt1ps0scar\r"
+#		}
+#	}
+#	interact
+#######################################################################################
+#			sleep 1
+#			rm -rf herpderp.tar.gz
+			sleep 1
 		echo Compression of current Wankercraft directory complete 
 		sleep 1
 		echo Removing Raw Minecraft directory #THIS PART REMOVES COPIED SERVER DIRECTORY
@@ -140,89 +122,4 @@ if [ $NOTIFY -eq 1 ]
 		screen -x $SCREEN -X stuff "`printf "say World of Wankercraft backup complete.\r"`"
 		sleep 1
 	echo World of Wankercraft backup complete
-fi
-
-
-# --Cron Job Install--
-#if [ $CRON -eq 1 ]
-#then
-#
-#  #Check if user can use crontab
-#   if [ -f "/etc/cron.allow" ]
-#   then
-#      EXIST=`grep $USER < /etc/cron.allow`
-#      if [ "$EXIST" != "$USER" ]
-#      then
-#         echo "Sorry. You are not allowed to edit cronjobs."
-#         exit
-#      fi
-#   fi
-
-   #Work out crontime
-   #if [ $BACKUPTIME -eq 0 -o $BACKUPTIME -lt 0 ]
-#   then
-#      MINS="*"
-#   else
-#      MINS="*/$BACKUPTIME"
-#   fi
-#
-   #Check if cronjob exists, if not then create.
-#   crontab -l > .crons
-#   EXIST=`crontab -l | grep $0 | cut -d";" -f2`
-#   CRONSET="$MINS * * * * cd $PWD;$0"
-
-#   if [ "$EXIST" == "$0" ]
-#   then
-
-      #Check if cron needs updating.
-#      THECRON=`crontab -l | grep $0`
-#      if [ "$THECRON" != "$CRONSET" ]
-#      then
-#         CRONS=`crontab -l | grep -v "$0"`
-#         echo "$CRONS" > .crons
-#         echo "$CRONSET" >> .crons
-#         crontab .crons
-#         echo "Cronjob has been updated"
-#      fi
-
-#      rm .crons
-#      exit
-#   else
-#      crontab -l > .crons
-#      echo "$CRONSET" >> .crons
-#      crontab .crons
-#      rm .crons
-#      echo "Autobackup has been installed."
-#      exit
-#   fi
-
-#fi
-
-
-
-#********************MAP MAKING SCRIPT********************
-
-if [ $MAP -eq 1 ]
-	then
-		if [ $NOTIFY -eq 1 ]
-			then
-				screen -x $SCREEN -X stuff "`printf "say Updating map. Things could get a bit laggy.\r"`"
-			sleep 1
-		fi
-		echo Updating world map
-		cd "$MAPPER"
-		python26 gmap.py --lighting --cachedir="$MAPCACHE" "$WORLD" "$MAPDIR"
-		sleep 1
-		screen -x $SCREEN -X stuff "`printf "say Finished updating the first map. Took long enough.\r"`"
-		sleep 2
-		python26 gmap.py --lighting --cachedir="$MAPCACHEALT" "$WORLDALT" "$MAPDIRALT"
-		sleep 1
-		screen -x $SCREEN -X stuff "`printf "say Finished updating the second map. Took long enough.\r"`"
-		sleep 1
-		echo World maps updated
-		echo World of Wankercraft Map has been updated
-	else 
-		screen -x $SCREEN -X stuff "`printf "say Map is not being updated this time.\r"`"
-		sleep 1
-		echo Map not being updated
 fi
